06b3dbe80c37553e5e52abc1a26c8ee1158dc09d
Roy Shi <royshi@meta.com>
2024-05-06 21:28:23 -0700

    Improve inline_functions by truly inline those functions

--- inline_functions/inline_for_bar.cpp
+++ inline_functions/inline_for_bar.cpp
@@ -1,5 +1,5 @@
 #include "bar.h"
 
-__attribute__ ((always_inline)) int get_bar_ret_inlined(const bar_ret& b) {
+__attribute__ ((always_inline)) static inline int get_bar_ret_inlined(const bar_ret& b) {
     return b.ret;
 }
--- inline_functions/inline_for_foo.h
+++ inline_functions/inline_for_foo.h
@@ -1,5 +1,5 @@
 #include "foo.h"
 
-__attribute__ ((always_inline)) int get_foo_ret_inlined(const foo_ret& f) {
+__attribute__ ((always_inline)) static inline int get_foo_ret_inlined(const foo_ret& f) {
     return f.ret;
 }
